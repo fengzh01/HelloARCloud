f2a8a84628818476bc96e23ca54497f17da74bd9
fengzhenhai <peter5091@163.com>
2018-09-11 16:46:54 +0800

    HelloAR

---
+++ app/src/main/java/com/demo/fzh/helloarcloudtest01/MainActivity.java
@@ -0,0 +1,26 @@
+package com.demo.fzh.helloarcloudtest01;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.test.compn.UnityPlayerActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_hello_ar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "hello AR!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, UnityPlayerActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
