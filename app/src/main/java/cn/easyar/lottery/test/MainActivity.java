91c7bed2776b5e72ef06d9c631d0ebbe62c3667b
fengzhenhai <peter5091@163.com>
2018-09-14 17:14:58 +0800

    修改applicationId

--- app/src/main/java/cn/easyar/lottery/test/MainActivity.java
+++ app/src/main/java/cn/easyar/lottery/test/MainActivity.java
@@ -1,11 +1,11 @@
-package cn.gov.lottery;
+package cn.easyar.lottery.test;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-import cn.gov.lottery.R;
+import cn.easyar.lottery.test.R;
 
 import cn.easyar.lottery.UnityPlayerActivity;
 
